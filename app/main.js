37d43b9d158fcd20dcbe98e904d46c9053fbffb3
JoanStark <47462893+JoanStark@users.noreply.github.com>
2019-02-10 11:14:16 +0100

    Add files via upload

---
+++ app/main.js
@@ -0,0 +1,6 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import MVT from 'ol/format/MVT';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
